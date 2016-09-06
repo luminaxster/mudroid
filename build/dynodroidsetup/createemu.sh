73fb4cc3c02f854adc414e074a901608993d6cfe
David Gonzalez <luminaxster@gmail.com>
2016-09-06 01:51:54 +0000

    large files added: *img

--- build/dynodroidsetup/createemu.sh
+++ build/dynodroidsetup/createemu.sh
@@ -2,4 +2,4 @@
 mkdir -p "/home/denglin1021/.android/avd/$1.avd"
 echo "target=android-10" > "/home/denglin1021/.android/avd/$1.ini"
 echo "path=/home/denglin1021/.android/avd/$1.avd" >> "/home/denglin1021/.android/avd/$1.ini"
-cp /home/denglin1021/dyno-droid/mudroid/build/dynodroidsetup/freshavd/emu.avd/* "/home/denglin1021/.android/avd/$1.avd"/
+cp /home/denglin1021/dyno-droid/dyno-droid/trunk/build/dynodroidsetup/freshavd/emu.avd/* "/home/denglin1021/.android/avd/$1.avd"/
